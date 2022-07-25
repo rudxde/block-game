e389a3d1a3b0580f5d5cca872a82f3d6ef5305a7
rudxde <kj4derechte@gmail.com>
2022-07-25 11:12:42 +0200

    feat: setup pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { GameComponent } from './components/game/game.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,13 @@ import { GameComponent } from './components/game/game.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 5 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:5000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
