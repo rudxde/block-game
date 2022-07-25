64dd7e29700ec1995235b888a87cde2a4c248371
rudxde <kj4derechte@gmail.com>
2022-07-25 14:00:47 +0200

    feat: add multi language support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './components/app/app.component';
 import { GameComponent } from './components/game/game.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { TranslocoRootModule } from './transloco-root.module';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoService } from '@ngneat/transloco';
 
 @NgModule({
   declarations: [
@@ -18,9 +21,24 @@ import { environment } from '../environments/environment';
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       registrationStrategy: 'registerImmediately'
-    })
+    }),
+    HttpClientModule,
+    TranslocoRootModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(translocoService: TranslocoService) {
+    // const userLang = navigator.language || navigator.userLanguage;
+    let language = navigator.language;
+    if(language.indexOf('-')) {
+      language = language.split('-')[0];
+    }
+    if (!translocoService.isLang(language)) {
+      return;
+    }
+    translocoService.setActiveLang(language);
+    document.querySelector('html')?.setAttribute('lang', translocoService.getActiveLang())
+  }
+}
