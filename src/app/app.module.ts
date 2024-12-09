8e9f929896cd43f08091f153ab56b9834817749a
rudxde <kj4derechte@gmail.com>
2024-12-09 22:20:46 +0100

    update angular material to v15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,18 +13,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
+import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
 import { SettingsComponent } from './components/settings/settings.component';
 import { MenuButtonComponent } from './components/menu-button/menu-button.component';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatLegacySnackBarModule as MatSnackBarModule } from '@angular/material/legacy-snack-bar';
 import { AppUpdateService } from './services/update.service';
 import { ErrorComponent } from './components/error/error.component';
 import { GlobalErrorHandler } from './services/global-error.service';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSliderModule } from '@angular/material/slider';
+import { MatLegacySlideToggleModule as MatSlideToggleModule } from '@angular/material/legacy-slide-toggle';
+import { MatLegacySliderModule as MatSliderModule } from '@angular/material/legacy-slider';
 import { ThemeService } from './services/theme.service';
 
 @NgModule({
