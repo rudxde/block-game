4fa3a9df0112822cdbad4a6a63fbf81778434e06
rudxde <kj4derechte@gmail.com>
2024-12-10 01:57:12 +0100

    fix/workaround the drawer bug

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, effect, OnInit, signal, ViewChild } from '@angular/core';
 import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 import { Router, RouterOutlet } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
@@ -11,23 +11,26 @@ import { MenuButtonComponent } from '../menu-button/menu-button.component';
 import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
-    imports: [MatSidenavModule, TranslocoModule, MenuButtonComponent, MatToolbarModule, MatButtonModule, MatIconModule, NgIf, RouterOutlet, AsyncPipe]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [MatSidenavModule, TranslocoModule, MenuButtonComponent, MatToolbarModule, MatButtonModule, MatIconModule, NgIf, RouterOutlet, AsyncPipe]
 })
 export class AppComponent implements OnInit {
 
-  @ViewChild('drawer') drawer!: MatDrawer;
+  drawerOpen = signal<boolean>(false);
+
+  // This is a workaround, since the sidenav wont open on the settings page for some reason, why this helps, i have no idea...
+  _ = effect(() => this.drawerOpen());
 
   title$: BehaviorSubject<string | undefined>;
   actionButton$: BehaviorSubject<IMenuBarActionButton | undefined>;
-  
+
   constructor(
     private router: Router,
     menuBarService: MenuBarService,
-  ) { 
+  ) {
     this.title$ = menuBarService.title;
     this.actionButton$ = menuBarService.actionButton;
   }
@@ -37,7 +40,11 @@ export class AppComponent implements OnInit {
 
   async openLink(path: string[]) {
     await this.router.navigate(path);
-    await this.drawer.toggle();
+    this.drawerOpen.set(false);
+  }
+
+  toggleDrawer() {
+    this.drawerOpen.set(!this.drawerOpen());
   }
 
 }
