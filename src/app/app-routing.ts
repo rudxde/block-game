7797cc87e156e3a4a734fb0bc0abf8b8d0836abc
rudxde <kj4derechte@gmail.com>
2024-12-10 01:20:59 +0100

    lazy load routes

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,12 +1,9 @@
 import { Routes } from '@angular/router';
-import { ErrorComponent } from './components/error/error.component';
-import { GameComponent } from './components/game/game.component';
-import { SettingsComponent } from './components/settings/settings.component';
 
 export const routes: Routes = [
-  { path: 'game', component: GameComponent },
-  { path: 'game/:mode', component: GameComponent },
-  { path: 'settings', component: SettingsComponent },
   { path: '', pathMatch: 'full', redirectTo: 'game' },
-  { path: '**', component: ErrorComponent, data: { code: 'notFound' } }
+  { path: 'game', loadComponent: async () => (await import('./components/game/game.component')).GameComponent },
+  { path: 'game/:mode', loadComponent: async () => (await import('./components/game/game.component')).GameComponent },
+  { path: 'settings', loadComponent: async () => (await import('./components/settings/settings.component')).SettingsComponent },
+  { path: '**', loadComponent: async () => (await import('./components/error/error.component')).ErrorComponent, data: { code: 'notFound' } }
 ];
